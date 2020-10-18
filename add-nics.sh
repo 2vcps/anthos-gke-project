c38234613fbe9425989cf28876a1aeb83b461574
Jon Owings <jon.owings@gmail.com>
2020-10-18 09:54:30 -0400

    test

---
+++ add-nics.sh
@@ -0,0 +1,6 @@
+for vm in $(govc ls vm | grep pool-1 | xargs -n1 basename)
+do
+       echo $vm
+       govc vm.network.add -vm $vm -net "iSCSI1" -net.adapter e1000e
+       govc device.info -vm $vm ethernet-*
+done
